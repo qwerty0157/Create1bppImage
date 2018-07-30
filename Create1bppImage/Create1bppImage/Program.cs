efcc0c34eb8139f157f3cd26bb9df73401d323b5
qwerty0157 <qwerty0157@gmail.com>
2018-07-30 23:33:54 +0900

    initial commit

---
+++ Create1bppImage/Create1bppImage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Create1bppImage
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
