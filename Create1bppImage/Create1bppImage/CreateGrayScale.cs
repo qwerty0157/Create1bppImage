84a420f805e874af3ba9f8553667a4a61d268730
qwerty0157 <qwerty0157@gmail.com>
2019-04-24 23:32:06 +0900

    add grayscale code

---
+++ Create1bppImage/Create1bppImage/CreateGrayScale.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.DrawingCore;
+using System.DrawingCore.Imaging;
+
+namespace Create1bppImage
+{
+    public class CreateGrayScale
+    {
+        public CreateGrayScale()
+        {}
+        /// <summary>
+        /// 指定した画像からグレースケール画像を作成する
+        /// </summary>
+        /// <param name="img">基の画像</param>
+        /// <returns>作成されたグレースケール画像</returns>
+        public static Image CreateGrayscaleImage(Image img)
+        {
+            //グレースケールの描画先となるImageオブジェクトを作成
+            Bitmap newImg = new Bitmap(img.Width, img.Height);
+            //newImgのGraphicsオブジェクトを取得
+            Graphics g = Graphics.FromImage(newImg);
+
+            //ColorMatrixオブジェクトの作成
+            //グレースケールに変換するための行列を指定する
+            System.DrawingCore.Imaging.ColorMatrix cm =
+                  new System.DrawingCore.Imaging.ColorMatrix(
+                new float[][]{
+            new float[]{0.3086f, 0.3086f, 0.3086f, 0 ,0},
+            new float[]{0.6094f, 0.6094f, 0.6094f, 0, 0},
+            new float[]{0.0820f, 0.0820f, 0.0820f, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1}
+                });
+            //ImageAttributesオブジェクトの作成
+            System.DrawingCore.Imaging.ImageAttributes ia =
+                  new System.DrawingCore.Imaging.ImageAttributes();
+            //ColorMatrixを設定する
+            ia.SetColorMatrix(cm);
+
+            //ImageAttributesを使用してグレースケールを描画
+            g.DrawImage(img,
+                new Rectangle(0, 0, img.Width, img.Height),
+                0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            //リソースを解放する
+            g.Dispose();
+
+            return newImg;
+        }
+    }
+}
--- Create1bppImage/Create1bppImage/Program.cs
+++ Create1bppImage/Create1bppImage/Program.cs
@@ -9,14 +9,22 @@ namespace Create1bppImage
     {
         public static void Main(string[] args)
         {
-            create1bppImage();
-            create1bppImageByErrorDiffusion();
+            createGrayScale();
+            //create1bppImage();
+            //create1bppImageByErrorDiffusion();
+        }
+
+        public static void createGrayScale()
+        {
+            var image = (Bitmap)Image.FromFile("../../../../input/circle.png");
+            var bmpImage = CreateGrayScale.CreateGrayscaleImage(image);
+            bmpImage.Save("../../../../input/grayscale.png");
         }
 
         public static void create1bppImage()
         {
-            var image = (Bitmap)Image.FromFile("../../../../input/wtnbyou.png");
-            var brightness = 0.80;
+            var image = (Bitmap)Image.FromFile("../../../../input/circle.png");
+            var brightness = 0.75;
             var bmpImage = Create1bppImage.Create1bppImagefunction(image, brightness); 
             bmpImage.Save("../../../../input/1bpp_wtnbyou.png");
         }
